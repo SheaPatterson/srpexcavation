68b69df7ce32a89da3a6bf46e675c76848010cf8
Shea Patterson <sheapatterson@icloud.com>
2022-11-13 18:07:42 -0500

    3

--- users-31083e.php
+++
@@ -1,3 +0,0 @@
-<?php
-    $users = array('shea' => array('id' => 1, 'name' => 'Shea', 'surname' => 'Patterson', 'pwd' => 'e1ZTEVUxCDd0ylKbnKWtiZgSxcC3JbIu', 'email' => 'shea@srpexcavation.com', 'info' => '', 'groups' => array('2','3')));
-    $salt = '$2y$10$demuvjxsSrYcihRtw7r1kl$';
